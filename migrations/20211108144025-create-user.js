3ec8d688410a130da444ea06aebda1e65d4c204c
Kirill <freshjs.ftd@gmail.com>
2021-11-08 17:03:50 +0200

    Add unfinished Sequelize configuration

---
+++ migrations/20211108144025-create-user.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING(64),
+        field: 'first_name',
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING(64),
+        field: 'last_name',
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING(64),
+        allowNull: false,
+      },
+      birthday: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      isMale: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at',
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
